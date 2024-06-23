988d73e2f77b30046941ed94fac951c49f41db4a
ZhongGuanbin <864376923@qq.com>
2024-06-24 02:11:31 +0800

    only add some framework codes

---
+++ lib/teaching_mass_spectrometer_fe.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+import 'package:teaching_mass_spectrometer_2nd/theme.dart';
+
+class TeachingMassSpectrometerFe extends StatelessWidget {
+  TeachingMassSpectrometerFe({super.key});
+
+  final _appTheme = AppTheme();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
